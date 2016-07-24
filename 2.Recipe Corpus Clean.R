f70bcbaa4eb3c275860b81a6a45b41b2638ef94d
kristopherdelane <kdstev@gmail.com>
2016-07-24 18:17:34 -0400

    Update 2.Recipe Corpus Clean.R

--- 2.Recipe Corpus Clean.R
+++ 2.Recipe Corpus Clean.R
@@ -1,39 +1,20 @@
+library(tm)
 ### The Beer Winners
 # Run this code after uploading RecipeData.Rda
 # Or if you ran Recipe Scraping-1
-library(tm)
 
 ### Code to tag the ID to the document
-### Will be important for our overall project
 myReader1 <- readTabular(mapping=list(content="ingredients", id="recipe"))
-myReaderCuisine <- readTabular(mapping=list(content="ingredients", id="cuisine"))
 
-### Create the corpus with document IDs
+### Remove punctuation and adding spacing for preprocessing
 data.clean <- data
 data.clean$ingredients<-gsub('"', "", data.clean$ingredients)
 data.clean$ingredients<-gsub(',', " ", data.clean$ingredients)
-
-### Corpus clean up tasks
-ingredient_text <- paste(data.clean$ingredients, collapse=" ")
-ingredient_source <- VectorSource(ingredient_text)
-corpus <- Corpus(ingredient_source)
-corpus <- tm_map(corpus, content_transformer(tolower))
-corpus <- tm_map(corpus, removePunctuation)
-corpus <- tm_map(corpus, stripWhitespace)
-
-### Create dtm to review
-dtm <- DocumentTermMatrix(corpus)
-dtm2 <- as.matrix(dtm)
-frequency <- colSums(dtm2)
-frequency <- sort(frequency, decreasing=TRUE)
-View(frequency)
-head(frequency)
-View(dtm2)
+data.clean$ingredients<-gsub("_"," ",data.clean$ingredients)
+data.clean$ingredients<-gsub("-","",data.clean$ingredients)
 
 ### Remove words that show up in ingredients list that are inefficient
-data.clean$ingredients<-gsub("-","",data.clean$ingredients)
-data.clean$ingredients<-gsub("_","",data.clean$ingredients)
-data.clean$ingredients<-gsub("kraft","",data.clean$ingredients)
+data.clean$ingredients<-gsub("Kraft","",data.clean$ingredients)
 data.clean$ingredients<-gsub("freshly","",data.clean$ingredients)
 data.clean$ingredients<-gsub("fresh","",data.clean$ingredients)
 data.clean$ingredients<-gsub("yolk","",data.clean$ingredients)
@@ -44,9 +25,7 @@ data.clean$ingredients<-gsub("kosher","",data.clean$ingredients)
 data.clean$ingredients<-gsub("unsalted","",data.clean$ingredients)
 data.clean$ingredients<-gsub("allpurpose","",data.clean$ingredients)
 data.clean$ingredients<-gsub("granulated","",data.clean$ingredients)
-data.clean$ingredients<-gsub("bonelessskinless","",data.clean$ingredients)
 data.clean$ingredients<-gsub("chopped","",data.clean$ingredients)
-data.clean$ingredients<-gsub("blackpepper","pepper",data.clean$ingredients)
 data.clean$ingredients<-gsub("shredded","",data.clean$ingredients)
 data.clean$ingredients<-gsub("light","",data.clean$ingredients)
 data.clean$ingredients<-gsub("crushed","",data.clean$ingredients)
@@ -63,22 +42,29 @@ data.clean$ingredients<-gsub("diced","",data.clean$ingredients)
 data.clean$ingredients<-gsub("boneless","",data.clean$ingredients)
 data.clean$ingredients<-gsub("frozen","",data.clean$ingredients)
 data.clean$ingredients<-gsub("dark","",data.clean$ingredients)
-data.clean$ingredients<-gsub("reducedsodium","",data.clean$ingredients)
+data.clean$ingredients<-gsub("reduced","",data.clean$ingredients)
 data.clean$ingredients<-gsub("canned","",data.clean$ingredients)
 data.clean$ingredients<-gsub("sliced","",data.clean$ingredients)
-data.clean$ingredients<-gsub("lowsodium","",data.clean$ingredients)
+data.clean$ingredients<-gsub("low","",data.clean$ingredients)
+data.clean$ingredients<-gsub("sodium","",data.clean$ingredients)
+data.clean$ingredients<-gsub("extra","",data.clean$ingredients)
+data.clean$ingredients<-gsub("virgin","",data.clean$ingredients)
 
-### Re-create dtm to review and frequency
+
+### Corpus clean up tasks
+corpus <- Corpus(DataframeSource(data.clean),readerControl=list(reader=myReader1))
+corpus <- tm_map(corpus, removePunctuation)
+
+### Create dtm to review
 dtm <- DocumentTermMatrix(corpus)
 dtm2 <- as.matrix(dtm)
 frequency <- colSums(dtm2)
 frequency <- sort(frequency, decreasing=TRUE)
-View(frequency)
 head(frequency)
-View(dtm2)
 
 ### View updated frequency and create dataframe
 View(frequency)
 freq.df <- as.data.frame(frequency)
 freq.df$ingredient <- row.names(freq.df)
 freq.df <- as.data.frame(freq.df, row.names = 1:length(freq.df$frequency))
+View(freq.df)
